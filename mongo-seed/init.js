caa3bd704aa8c5c7dd7cc9351c9d554895c97d51
Tomek <tomasz.czarny.czarnecki@gmail.com>
2020-12-24 06:22:53 +0100

    Seeding multiple collections in one database from sh script

---
+++ mongo-seed/init.js
@@ -0,0 +1,27 @@
+db.people.drop();
+db.people.insertMany([
+    {
+        _id: 0,
+        name: "pierwszy",
+        password: "lkjghfds",
+        age: 1
+    },
+    {
+        _id: 1,
+        name: "drugi",
+        password: "koniecznie",
+        age: 13
+    },
+    {
+        _id: 2,
+        name: "trzeci",
+        password: "koniecznie",
+        age: 13
+    },
+    {
+        _id: 4,
+        name: "drugi",
+        password: "halemba",
+        age: 13
+    }
+])
--- mongo-seed/start.sh
+++ mongo-seed/start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-sleep 30
-mongoimport --host api-database --db humans --collection people --type json --file /init.json --jsonArray
+
+mongoimport --host api-database --db humans --collection people --type json --file /mongo-seed/people.json --jsonArray
+mongoimport --host api-database --db humans --collection counters --type json --file /mongo-seed/counters.json --jsonArray
